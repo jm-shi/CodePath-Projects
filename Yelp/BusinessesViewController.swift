49835f34291e008d27adc36a24a1c8a399a8b037
Jamie S <jamieshi2u@gmail.com>
2017-02-17 01:19:42 -0800

    Implemented search

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var searchBar = UISearchBar()
+    
     var businesses: [Business]!
+    var filteredBusinesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,20 +24,12 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
+    
+        self.searchBar.delegate = self
+        self.searchBar.tintColor = UIColor.black
+        self.navigationItem.titleView = self.searchBar
         
-        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
-            self.businesses = businesses
-            self.tableView.reloadData()
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
-                }
-            }
-            
-            }
-        )
+        resetSearch()
         
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
@@ -54,10 +49,23 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         // Dispose of any resources that can be recreated.
     }
     
+    func resetSearch() {
+        Business.searchWithTerm(term: "", completion: { (businesses: [Business]?, error: Error?) -> Void in
+         
+         self.businesses = businesses
+         self.tableView.reloadData()
+         })
+    }
     
+    // MARK: tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if businesses != nil {
-            return businesses!.count
+            if (searchBar.text?.isEmpty)! {
+                return businesses!.count
+            }
+            else {
+                return filteredBusinesses!.count
+            }
         }
         else {
             return 0
@@ -67,9 +75,42 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
         
-        cell.business = businesses[indexPath.row]
+        if (searchBar.text?.isEmpty)! {
+            cell.business = businesses[indexPath.row]
+        }
+        else {
+            cell.business = filteredBusinesses[indexPath.row]
+        }
         
         return cell
+    }
+    
+    // MARK: searchBar
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        resetSearch()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredBusinesses = businesses
+        }
+        else {
+            filteredBusinesses = businesses.filter({ (business: Business) -> Bool in
+                if let name = business.name {
+                    return name.range(of: searchText, options: .caseInsensitive) != nil
+                }
+                return false
+            })
+        }
+        
+        tableView.reloadData()
     }
     
     /*
