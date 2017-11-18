9fd024d5182843c8b13766581c478894fdbe5099
Jamie S <jamieshi2u@gmail.com>
2017-11-18 13:18:25 -0800

    Completed lab 2

--- Tumblr/PhotoCell.swift
+++ Tumblr/PhotoCell.swift
@@ -14,13 +14,11 @@ class PhotoCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        photo.contentMode = .scaleAspectFit
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -63,15 +63,26 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
-
-    /*
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        let vc = segue.destination as! PhotoDetailsViewController
+ 
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        
+        let post = posts[(indexPath?.row)!]
+        let photos = post["photos"] as? [[String: Any]]
+        let photo = photos?[0]
+        let originalSize = photo?["original_size"] as! [String: Any]
+        let urlString = originalSize["url"] as! String
+        vc.photoURL = urlString
     }
-    */
+    
 
 }
